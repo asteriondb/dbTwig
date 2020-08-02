9b00c9e9b01cd12006f1deea0fc84f43fd5b78e1
JumpinJackFlash <s.guilford@asteriondb.com>
2020-08-01 17:28:18 -0700

    Accept a parameter to specify location of spool file.

--- dba/oracle/extractDbTwigData.sql
+++ dba/oracle/extractDbTwigData.sql
@@ -6,7 +6,7 @@ SET LINESIZE 20000
 set trimspool on
 set termout off
 
-spool dbTwigData.sql
+spool &1
 
 select  'insert into middle_tier_map values ('''||entry_point||''', '''||object_type||''', '''||object_name||''');'
   from  middle_tier_map;
