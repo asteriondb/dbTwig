4bc8d3229d585455dc9c1c2dac0f00a0c13a1aaf
JumpinJackFlash <s.guilford@asteriondb.com>
2020-10-30 18:52:44 -0700

    Use a settings.sh file.

--- devops/extractObjects.sh
+++ devops/extractObjects.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. $HOME/devops/settings.sh
+
 function wrappedCheck()
 {
   echo 'Checking '$1' for wrapped code.'
@@ -15,16 +17,16 @@ set -e
 
 cd "$(dirname "$0")"
 
-sqlplus dbtwig/dbtwig@local-dev @extractPackageHeader db_twig ../dba/oracle 
-sqlplus dbtwig/dbtwig@local-dev @preWrapPackageBody db_twig ../dba/oracle
+sqlplus $DBTWIG_USER/$DBTWIG_PASS@$DB_NAME @extractPackageHeader db_twig ../dba/oracle 
+sqlplus $DBTWIG_USER/$DBTWIG_PASS@$DB_NAME @preWrapPackageBody db_twig ../dba/oracle
 wrappedCheck ../dba/oracle/db_twig.pls
 $SQLPATH/end_package_input.sh >>../dba/oracle/db_twig.pls
 $SQLPATH/show_errors.sh db_twig >>../dba/oracle/db_twig.pls 
 
-sqlplus dbtwig_tutorials/dbtwig_tutorials@local-dev @extractPackageHeader react_example /jjflash/git/asterionDB/dbTwig/examples/react/dba
-sqlplus dbtwig_tutorials/dbtwig_tutorials@local-dev @preWrapPackageBody react_example /jjflash/git/asterionDB/dbTwig/examples/react/dba
+sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @extractPackageHeader react_example /jjflash/git/asterionDB/dbTwig/examples/react/dba
+sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @preWrapPackageBody react_example /jjflash/git/asterionDB/dbTwig/examples/react/dba
 wrappedCheck /jjflash/git/asterionDB/dbTwig/examples/react/dba/react_example.pls
 $SQLPATH/end_package_input.sh >>../examples/react/dba/react_example.pls
 $SQLPATH/show_errors.sh react_example >>../examples/react/dba/react_example.pls 
 
-sqlplus dbtwig_tutorials/dbtwig_tutorials@local-dev @/jjflash/git/asterionDB/dbTwig/dba/oracle/extractDbTwigData /jjflash/git/asterionDB/dbTwig/examples/react/dba/dbTwigData.sql reactExample
+sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @/jjflash/git/asterionDB/dbTwig/dba/oracle/extractDbTwigData /jjflash/git/asterionDB/dbTwig/examples/react/dba/dbTwigData.sql reactExample
