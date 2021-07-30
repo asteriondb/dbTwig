21a4eca8193169fc3bbca5a70cd43b271aeae241
Steve Guilford <gilly@jjflash.asteriondb.local>
2021-07-30 13:51:43 -0700

    New directory layout.

--- devops/extractObjects.sh
+++ devops/extractObjects.sh
@@ -17,16 +17,16 @@ set -e
 
 cd "$(dirname "$0")"
 
-sqlplus $DBTWIG_USER/$DBTWIG_PASS@$DB_NAME @extractPackageHeader db_twig ../dba/oracle 
-sqlplus $DBTWIG_USER/$DBTWIG_PASS@$DB_NAME @preWrapPackageBody db_twig ../dba/oracle
+sqlplus $DBTWIG_USER/$DBTWIG_PASS@$DB_NAME @extractPackageHeader db_twig ../dba 
+sqlplus $DBTWIG_USER/$DBTWIG_PASS@$DB_NAME @preWrapPackageBody db_twig ../dba
 wrappedCheck ../dba/oracle/db_twig.pls
-$SQLPATH/end_package_input.sh >>../dba/oracle/db_twig.pls
-$SQLPATH/show_errors.sh db_twig >>../dba/oracle/db_twig.pls 
+$SQLPATH/end_package_input.sh >>../dba/db_twig.pls
+$SQLPATH/show_errors.sh db_twig >>../dba/db_twig.pls 
 
-sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @extractPackageHeader react_example /storage/git/dbTwig/examples/react/dba
-sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @preWrapPackageBody react_example /storage/git/dbTwig/examples/react/dba
+sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @extractPackageHeader react_example ../examples/react/dba
+sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @preWrapPackageBody react_example ../examples/react/dba
 wrappedCheck /storage/git/dbTwig/examples/react/dba/react_example.pls
 $SQLPATH/end_package_input.sh >>../examples/react/dba/react_example.pls
 $SQLPATH/show_errors.sh react_example >>../examples/react/dba/react_example.pls 
 
-sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @/storage/git/dbTwig/dba/oracle/extractDbTwigData /storage/git/dbTwig/examples/react/dba/dbTwigData.sql reactExample
+sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @/home/asterion/git/dbTwig/dba/extractDbTwigData /home/asterion/git/dbTwig/examples/react/dba/dbTwigData.sql reactExample
