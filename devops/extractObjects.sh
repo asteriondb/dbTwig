17b2259f47d168d699579729df89076c4737b505
Steve Guilford <gilly@jjflash.asteriondb.local>
2021-07-31 20:59:40 -0700

    Build box stuff.

--- devops/extractObjects.sh
+++ devops/extractObjects.sh
@@ -17,16 +17,18 @@ set -e
 
 cd "$(dirname "$0")"
 
-sqlplus $DBTWIG_USER/$DBTWIG_PASS@$DB_NAME @extractPackageHeader db_twig ../dba 
-sqlplus $DBTWIG_USER/$DBTWIG_PASS@$DB_NAME @preWrapPackageBody db_twig ../dba
-wrappedCheck ../dba/oracle/db_twig.pls
-$SQLPATH/end_package_input.sh >>../dba/db_twig.pls
-$SQLPATH/show_errors.sh db_twig >>../dba/db_twig.pls 
+sqlplus $DBTWIG_USER/$DBTWIG_PASS@$DB_NAME @extractPackageHeader db_twig $GIT_HOME/dbTwig/dba 
+sqlplus $DBTWIG_USER/$DBTWIG_PASS@$DB_NAME @preWrapPackageBody db_twig $GIT_HOME/dbTwig/dba
+wrappedCheck $GIT_HOME/dbTwig/dba/db_twig.pls
+$SQLPATH/end_package_input.sh >>$GIT_HOME/dbTwig/dba/db_twig.pls
+$SQLPATH/show_errors.sh db_twig >>$GIT_HOME/dbTwig/dba/db_twig.pls 
 
-sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @extractPackageHeader react_example ../examples/react/dba
-sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @preWrapPackageBody react_example ../examples/react/dba
-wrappedCheck /storage/git/dbTwig/examples/react/dba/react_example.pls
-$SQLPATH/end_package_input.sh >>../examples/react/dba/react_example.pls
-$SQLPATH/show_errors.sh react_example >>../examples/react/dba/react_example.pls 
+wrap iname=$GIT_HOME/dbTwig/dba/db_twig.pls oname=$GIT_HOME/dbTwig/dba/db_twig.plb
 
-sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @/home/asterion/git/dbTwig/dba/extractDbTwigData /home/asterion/git/dbTwig/examples/react/dba/dbTwigData.sql reactExample
+sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @extractPackageHeader react_example $GIT_HOME/dbTwig/examples/react/dba
+sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @preWrapPackageBody react_example $GIT_HOME/dbTwig/examples/react/dba
+wrappedCheck $GIT_HOME/dbTwig/examples/react/dba/react_example.pls
+$SQLPATH/end_package_input.sh >>$GIT_HOME/dbTwig/examples/react/dba/react_example.pls
+$SQLPATH/show_errors.sh react_example >>$GIT_HOME/dbTwig/examples/react/dba/react_example.pls 
+
+sqlplus $TUTORIAL_USER/$TUTORIAL_PASS@$DB_NAME @$GIT_HOME/dbTwig/dba/extractDbTwigData $GIT_HOME/dbTwig/examples/react/dba/dbTwigData.sql reactExample
