c43a2a16816232e989f51d06aa36b4aa3a089145
Steve Guilford <gilly@jjflash.asteriondb.local>
2022-05-14 16:15:47 -0700

    Update devops.

--- devops/extractObjects.sh
+++ devops/extractObjects.sh
@@ -4,14 +4,11 @@
 
 set -e
 
-cd "$(dirname "$0")"
+cd $GIT_HOME/dbTwig/dba
 
-sqlplus $DBTWIG_USER/$DBTWIG_PASS@$DB_NAME @extractObjects $GIT_HOME $TUTORIAL_USER $TUTORIAL_PASS $DB_NAME
+sqlplus $DBTWIG_USER/$DBTWIG_PASS@$DB_NAME @extractObjects $GIT_HOME
 
 $SQLPATH/end_package_input.sh >>$GIT_HOME/dbTwig/dba/db_twig.pls
 $SQLPATH/show_errors.sh db_twig >>$GIT_HOME/dbTwig/dba/db_twig.pls 
-
-$SQLPATH/end_package_input.sh >>$GIT_HOME/dbTwig/examples/react/dba/react_example.pls
-$SQLPATH/show_errors.sh react_example >>$GIT_HOME/dbTwig/examples/react/dba/react_example.pls 
 
 
