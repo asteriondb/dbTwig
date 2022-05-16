a3078562cc375bd0a8c5bee40b0e29b3377820f8
Steve Guilford <gilly@jjflash.asteriondb.local>
2022-05-16 15:23:04 -0700

    Ordered...

--- examples/react/dba/dbTwigData.sql
+++ examples/react/dba/dbTwigData.sql
@@ -1,4 +1,4 @@
-insert into middle_tier_map values ('getSupportInfo', 'function', 'react_example.get_support_info', 'reactExample', 'none', 'N');
-insert into middle_tier_map values ('getInsuranceClaims', 'function', 'react_example.get_insurance_claims', 'reactExample', 'none', 'N');
 insert into middle_tier_map values ('getInsuranceClaimDetail', 'function', 'react_example.get_insurance_claim_detail', 'reactExample', 'none', 'N');
+insert into middle_tier_map values ('getInsuranceClaims', 'function', 'react_example.get_insurance_claims', 'reactExample', 'none', 'N');
+insert into middle_tier_map values ('getSupportInfo', 'function', 'react_example.get_support_info', 'reactExample', 'none', 'N');
 insert into middle_tier_map values ('restApiError', 'function', 'react_example.error_handler', 'reactExample', 'none', 'N');
