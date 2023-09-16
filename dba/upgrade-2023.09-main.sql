93244891a2ad261603826eeeed982178f932ff61
Steve Guilford <s.guilford@asteriondb.com>
2023-09-16 16:46:45 -0700

    P-Release

---
+++ dba/upgrade-2023.09-main.sql
@@ -0,0 +1,31 @@
+/*
+
+Upgrade Notes
+
+*/
+
+define  s_git_tag  = 'rc2023.03'
+define  s_git_branch = 'main'
+
+spool upgrade-&s_git_tag-&s_git_branch..log
+
+whenever sqlerror exit
+connect &1/&2;
+
+prompt
+accept dbtwig prompt "Enter the name of the user that owns the DbTwig schema [dbtwig]: " default dbtwig
+prompt
+
+alter session set current_schema = &&dbtwig;
+
+whenever sqlerror continue
+
+REM  Put stuff between here.....
+
+
+REM  ...and here
+
+prompt 
+spool off;
+
+exit;
