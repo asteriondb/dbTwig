e0ed656670876cc5039ffae8c00529d63289972d
Steve Guilford <s.guilford@asteriondb.com>
2024-01-12 13:09:46 -0800

    No need to install serve in this script.

--- examples/admin/install.sh
+++ examples/admin/install.sh
@@ -5,7 +5,4 @@ set -e
 cd ../dba
 sqlplus /nolog @install
 
-echo "Installing Node-JS serve program...sudo access required..."
-sudo npm install -g serve
-
 echo "Installation successful. You can run the demo web-app with 'serve -s -l 5000 build' from the reactDemo subdirectory."
