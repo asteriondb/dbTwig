9e528f06d9897057fce96e6472dbd2ed854c8c7b
Steve Guilford <gilly@hunter.asteriondb.local>
2025-04-12 20:13:59 -0700

    Next release.

---
+++ dba/upgrade-rc2025.09-main.sql
@@ -0,0 +1,31 @@
+/*
+
+
+
+*/
+
+define dbtwig_user = '&1'
+
+spool $HOME/asterion/oracle/dbTwig/dba/upgrade-rc2025.09-main.log
+whenever sqlerror exit
+
+set echo on
+
+alter session set current_schema = &dbtwig_user;
+whenever sqlerror continue
+
+REM  Put stuff between here.....
+
+
+
+REM  ...and here
+
+@$HOME/asterion/oracle/dbTwig/dba/db_twig
+@$HOME/asterion/oracle/dbTwig/dba/db_twig.pls
+
+@$HOME/asterion/oracle/dbTwig/dba/call_restapi
+@$HOME/asterion/oracle/dbTwig/dba/get_column_length
+
+prompt 
+spool off;
+
